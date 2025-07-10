06afc82de2efe91dd00875d8995b5c9fa48e213a
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-07-10 16:54:23 +0300

    feat: add loader function

---
+++ src/shared/ui/loader.tsx
@@ -0,0 +1,9 @@
+import { RiLoader4Line } from 'react-icons/ri';
+
+export default function Loader() {
+	return (
+		<div className='flex items-center justify-center p-10'>
+			<RiLoader4Line className='h-8 w-8 animate-spin text-muted-foreground' />
+		</div>
+	);
+}
