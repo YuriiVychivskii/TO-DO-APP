26b6f7744fbbdb10c96fce9bfeaee5f4a6257070
Yura Vyvhivskii <yuravychivskii@gmail.com>
2025-07-10 18:45:03 +0300

    fix: delete padding

--- src/shared/ui/loader.tsx
+++ src/shared/ui/loader.tsx
@@ -2,7 +2,7 @@ import { RiLoader4Line } from 'react-icons/ri';
 
 export default function Loader() {
 	return (
-		<div className='flex items-center justify-center p-10'>
+		<div className='flex items-center justify-center'>
 			<RiLoader4Line className='h-8 w-8 animate-spin text-muted-foreground' />
 		</div>
 	);
